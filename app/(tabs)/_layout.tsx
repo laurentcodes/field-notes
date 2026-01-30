2f455b5cd037712829484b0a4018be8e592f3195
Saint Laurent <stlaurentcodes@gmail.com>
2026-01-30 12:52:30 +0100

    fix: fix native tab behavior on older ios

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,36 +1,36 @@
-import { NativeTabs, Label, Icon } from 'expo-router/unstable-native-tabs';
-import { useThemeColor } from 'heroui-native';
+import { NativeTabs, Label, Icon } from "expo-router/unstable-native-tabs";
+import { useThemeColor } from "heroui-native";
 
 export default function TabLayout() {
-  const [background, accent] = useThemeColor(['background', 'accent']);
+	const [background, accent] = useThemeColor(["background", "accent"]);
 
-  return (
-    <NativeTabs tintColor={accent}>
-      <NativeTabs.Trigger
-        name='index'
-        options={{
-          backgroundColor: background,
-        }}
-      >
-        <Label>Notes</Label>
-        <Icon
-          sf={{ default: 'note.text', selected: 'note.text' }}
-          drawable='ic_menu_edit'
-        />
-      </NativeTabs.Trigger>
+	return (
+		<NativeTabs tintColor={accent} disableTransparentOnScrollEdge>
+			<NativeTabs.Trigger
+				name="index"
+				options={{
+					backgroundColor: background,
+				}}
+			>
+				<Label>Notes</Label>
+				<Icon
+					sf={{ default: "note.text", selected: "note.text" }}
+					drawable="ic_menu_edit"
+				/>
+			</NativeTabs.Trigger>
 
-      <NativeTabs.Trigger
-        name='settings'
-        options={{
-          backgroundColor: background,
-        }}
-      >
-        <Label>Settings</Label>
-        <Icon
-          sf={{ default: 'gearshape', selected: 'gearshape.fill' }}
-          drawable='ic_settings'
-        />
-      </NativeTabs.Trigger>
-    </NativeTabs>
-  );
+			<NativeTabs.Trigger
+				name="settings"
+				options={{
+					backgroundColor: background,
+				}}
+			>
+				<Label>Settings</Label>
+				<Icon
+					sf={{ default: "gearshape", selected: "gearshape.fill" }}
+					drawable="ic_settings"
+				/>
+			</NativeTabs.Trigger>
+		</NativeTabs>
+	);
 }
